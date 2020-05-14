f009091f8053c0c23f8c3fbf96fd8baa4172c040
huseyin <thehusko@gmail.com>
2020-05-14 15:06:51 +0100

    added small tests for the app component and view component

---
+++ src/tests/viewRecipe.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {shallow} from "enzyme"
+import ViewRecipe from "../components/viewRecipe"
+
+describe("View Page", () =>{
+    const wrapper = shallow(<ViewRecipe />)
+    beforeEach()
+    it('calls componentDidMount', () => {
+        const spy = jest.spyOn(ViewRecipe.prototype, 'componentDidMount');
+        wrapper.instance().componentDidMount();
+        expect(spy).toHaveBeenCalled();;
+    })
+
+    /* it("recipeData state changes", () =>{
+        wrapper.instance().getRecipes()
+        expect(wrapper.state("recipeData")).toEqual("BBQ Wings")
+    }) */
+})
