cc679b095d8c69d586e84387043951354f2c09a1
huseyin <thehusko@gmail.com>
2020-05-14 14:24:42 +0100

    added some routes, get all works

--- server.js
+++ server.js
@@ -2,23 +2,19 @@ const express = require('express');
 const PORT = process.env.PORT  || 3000 ;
 // const path = require("path");
 const app = express();
+const recipes = require("./routes/recipeRoutes")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:', '*');
+  res.header('Access-Control-Allow-Origin', "*");
   next();
 });
 
-app.use("/", function(req,res){
-    res.send("hi")
-})
+app.use("/", recipes)
 
-app.get("/test", function(req,res){
-  res.json({name:"john",age:23})
-})
 
 
 app.listen(PORT, () => {
